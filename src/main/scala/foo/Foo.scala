944b3ab9cc2e219a1c7880883ce14b9b1b0f79c9
TATSUNO Yasuhiro <ytatsuno.jp@gmail.com>
2022-12-17 22:00:25 +0900

    Init

--- src/main/scala/foo/Foo.scala
+++ src/main/scala/foo/Foo.scala
@@ -3,7 +3,7 @@ package foo
 sealed trait Foo
 
 object Foo {
-  object LogOrdering extends Ordering[Foo] {
+  implicit object LogOrdering extends Ordering[Foo] {
     override def compare(x: Foo, y: Foo): Int = ???
   }
 }
